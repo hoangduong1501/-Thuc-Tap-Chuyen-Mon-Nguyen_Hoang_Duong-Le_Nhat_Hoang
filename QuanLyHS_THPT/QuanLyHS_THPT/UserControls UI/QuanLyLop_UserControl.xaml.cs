e33df8e4f6a6e03c416a46128d05fe808e1ade71
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-04 20:49:12 +0700

    Create QuanLyLop_UserControl.xaml.cs

---
+++ QuanLyHS_THPT/QuanLyHS_THPT/UserControls UI/QuanLyLop_UserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyHS_THPT.UserControls_UI
+{
+    /// <summary>
+    /// Interaction logic for QuanLyLop_UserControl.xaml
+    /// </summary>
+    public partial class QuanLyLop_UserControl : UserControl
+    {
+        public QuanLyLop_UserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_PhanLop_Click(object sender, RoutedEventArgs e)
+        {
+            UserControls_UI.PhanLop_UserControl phanLop_UserControl = new PhanLop_UserControl();
+            this.Grid_Person.Children.Add(phanLop_UserControl);
+        }
+    }
+}
