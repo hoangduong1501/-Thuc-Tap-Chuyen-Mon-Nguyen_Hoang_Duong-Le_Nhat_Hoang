d3c27641fef23f3af4ba5b5ab3e745db9f07d95d
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-04 20:45:42 +0700

    Create SuaGiaoVien_Window.xaml.cs

---
+++ QuanLyHS_THPT/QuanLyHS_THPT/Controls UI/SuaGiaoVien_Window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyHS_THPT.Controls_UI
+{
+    /// <summary>
+    /// Interaction logic for SuaGiaoVien_Window.xaml
+    /// </summary>
+    public partial class SuaGiaoVien_Window : Window
+    {
+        public SuaGiaoVien_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Chip_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
