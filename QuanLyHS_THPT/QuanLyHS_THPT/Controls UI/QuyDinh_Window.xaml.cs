844f89c6223e38b6e0e44db98ffc9e40942cdd21
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-04 20:45:32 +0700

    Create QuyDinh_Window.xaml.cs

---
+++ QuanLyHS_THPT/QuanLyHS_THPT/Controls UI/QuyDinh_Window.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyHS_THPT.Controls_UI
+{
+    /// <summary>
+    /// Interaction logic for QuyDinh_Window.xaml
+    /// </summary>
+    public partial class QuyDinh_Window : Window
+    {
+        public QuyDinh_Window()
+        {
+            InitializeComponent();
+            Create_Item();
+        }
+
+        private void Create_Item()
+        {
+            List<int> lst_Item = new List<int>();
+            for(int i = 0; i <= 100; i++)
+            {
+                lst_Item.Add(i);
+            }
+
+            cbb_SLMin.ItemsSource = lst_Item;
+            cbb_SLMax.ItemsSource = lst_Item;
+            cbb_TuoiMax.ItemsSource = lst_Item;
+            cbb_TuoiMin.ItemsSource = lst_Item;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
