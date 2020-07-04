c3127bf3f687c404b17ea9ed2884b2a46de900eb
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-04 20:48:54 +0700

    Create NamHoc_UserControl.xaml.cs

---
+++ QuanLyHS_THPT/QuanLyHS_THPT/UserControls UI/NamHoc_UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyHS_THPT.UserControls_UI
+{
+    /// <summary>
+    /// Interaction logic for NamHoc_UserControl.xaml
+    /// </summary>
+    public partial class NamHoc_UserControl : UserControl
+    {
+        public NamHoc_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
