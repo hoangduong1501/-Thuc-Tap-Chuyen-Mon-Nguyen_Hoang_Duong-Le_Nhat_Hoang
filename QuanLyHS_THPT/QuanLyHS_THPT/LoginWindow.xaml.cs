590c1c22f6287ab3384b2d4fde91993a63640bff
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-04 20:45:50 +0700

    Create LoginWindow.xaml.cs

---
+++ QuanLyHS_THPT/QuanLyHS_THPT/LoginWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyHS_THPT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Controls_UI.AdminWindow adminWindow = new Controls_UI.AdminWindow();
+            this.Visibility = Visibility.Collapsed;
+            adminWindow.ShowDialog();
+            this.Visibility = Visibility.Visible;
+        }
+    }
+}
