774aa3181fcacfb9c9bd7b82ed8fb085500a2229
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2020-07-09 09:51:05 +0700

    Cap nhat 09/07/2020 09:51

--- QuanLyHS_THPT/QuanLyHS_THPT/View Model/VM_NguoiDung.cs
+++ QuanLyHS_THPT/QuanLyHS_THPT/View Model/VM_NguoiDung.cs
@@ -1,4 +1,5 @@
-﻿using QuanLyHS_THPT.models;
+﻿using QuanLyHS_THPT.Data;
+using QuanLyHS_THPT.models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -42,6 +43,55 @@ namespace QuanLyHS_THPT.View_Model
             nguoiDung.tenDangNhap = dataTable.Rows[0][3].ToString();
             nguoiDung.matKhauDangNhap = dataTable.Rows[0][4].ToString();
             nguoiDung.loaiNguoiDung = dataTable.Rows[0][6].ToString();
+        }
+
+        public List<NguoiDung_Class> DanhSach_NguoiDung()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable = LayDS_Query(@"EXEC dbo.LayDS_NguoiDung");
+
+            List<NguoiDung_Class> lst = new List<NguoiDung_Class>();
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                lst.Add(new NguoiDung_Class()
+                {
+                    maNguoiDung = dataRow[0].ToString().Trim(),
+                    loaiNguoiDung = dataRow[1].ToString(),
+                    tenNguoiDung = dataRow[2].ToString().Trim(),
+                    tenDangNhap = dataRow[3].ToString().Trim(),
+                    matKhauDangNhap = dataRow[4].ToString().Trim()
+                });
+            }
+
+            return lst;
+        }
+
+        public List<LoaiNguoiDung_Class> DanhSach_LoaiNguoiDung()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable = LayDS_Query(@"EXEC LayDS_LoaiNguoiDung");
+
+            List<LoaiNguoiDung_Class> lst = new List<LoaiNguoiDung_Class>();
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                lst.Add(new LoaiNguoiDung_Class()
+                {
+                    ma_Loai = dataRow[0].ToString().Trim(),
+                    ten_Loai = dataRow[1].ToString()
+                });
+            }
+
+            return lst;
+        }
+
+        private DataTable LayDS_Query(string query)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandText = query;
+
+            return Exec_Class.GetData(sqlCommand);
         }
     }
 }
